4a4d551978b8eff66ba363444d049c0188ccf79d
Mauricio Henao <atomauro@cosmos-2.local>
2022-09-15 14:30:47 -0500

    .

--- src/components/SpreadMap/index.jsx
+++ src/components/SpreadMap/index.jsx
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 const SpreadMap = ({Wwhitepaper , SectionIcon11}) => {
 
   return (
@@ -17,10 +19,22 @@ const SpreadMap = ({Wwhitepaper , SectionIcon11}) => {
               </div>
               <h4 className="text-white">Download our Wallet</h4>
               <p className="text-white">In order to interact with our platform you need a wallet capable of using the Polygon Network, there are many options on the market, we directly suggest our multi-chain guarded wallet. Here you can centralize your funds and receive constant benefits.</p>
-              <a className="btn dream-btn mt-30" href="#">
+              <a className="btn dream-btn mt-30" onClick={() => {
+        Swal.fire({
+          text: 'Stay tune',
+          title: 'Coming soon!',
+          icon: 'success'
+        })
+      }}>
                 Get Android
               </a>
-              <a className="btn dream-btn mt-30 ml-5" href="#">
+              <a className="btn dream-btn mt-30 ml-5" onClick={() => {
+        Swal.fire({
+          text: 'Stay tune',
+          title: 'Coming soon!',
+          icon: 'success'
+        })
+      }}>
               Get IOs
               </a>
             </div>
