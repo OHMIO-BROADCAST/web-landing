16105052c2ee21e863a547af7bbf63ce6230623a
Mauricio Henao <atomauro@cosmos-2.local>
2022-09-22 09:27:19 -0500

    .

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -33,7 +33,7 @@ const SecHeroSection = ({
                   <h3 className="special-head dark">{specialHead}</h3>
                 </div>
                 <h1>{title}</h1>
-                <p className="w-text fadeInUp" data-wow-delay="0.3s"><text style={{textDecoration:'underline'}}>OHMIO</text> is the solution for Broadcast Industry, using Blockchain technology and Internet of things (IoT). <text style={{textDecoration:'underline'}}>OHMIOBOX</text> makes possible tokenizing the efficiency and rating on transmission stations over the world.</p>
+                <p className="w-text fadeInUp" data-wow-delay="0.3s"><text style={{textDecoration:'underline'}}>OHMIO</text> is the solution for Broadcast Industry, using Blockchain technology and Internet of things (IoT). <text style={{textDecoration:'underline'}}>OHMIOBOX</text> makes possible tokenizing the efficiency and rating of transmission stations over the world.</p>
                 <div className="dream-btn-group fadeInUp" data-wow-delay="0.4s" style={{display:'flex', justifyContent:'center'}}>
                   <a href="https://ohmio.gitbook.io/" target="_blank" className="btn more-btn mr-3">{link1}</a>
                   <a href="https://app.ohmiobroadcast.io/" target="_blank" className="btn more-btn-app">{link2}</a>
